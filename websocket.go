1631cea096eb6e42d5f858f97473002e5f098a36
Srayan Jana <valorzard.kiko@outlook.com>
2024-10-11 16:17:35 -0700

    got it working

--- websocket.go
+++ websocket.go
@@ -3,21 +3,15 @@ package main
 import (
 	"bytes"
 	"context"
-	"fmt"
 	"io"
 	"log"
 	"net/http"
-	"time"
-
-	"golang.org/x/time/rate"
 
 	"github.com/coder/websocket"
 )
 
 func websocketHandler(w http.ResponseWriter, r *http.Request) {
-	conn, err := websocket.Accept(w, r, &websocket.AcceptOptions{
-		Subprotocols: []string{"echo"},
-	})
+	conn, err := websocket.Accept(w, r, nil)
 	if err != nil {
 		log.Printf("websocket initialization error: %s", err)
 		return
@@ -28,55 +22,35 @@ func websocketHandler(w http.ResponseWriter, r *http.Request) {
 	// Do something with conn C.
 	defer conn.CloseNow()
 
-	if conn.Subprotocol() != "echo" {
-		conn.Close(websocket.StatusPolicyViolation, "client must speak the echo subprotocol")
-		return
-	}
+	ctx := context.Background()
 
-	limiter := rate.NewLimiter(rate.Every(time.Millisecond*100), 10)
-	for {
-		err = echo(conn, limiter)
-		if websocket.CloseStatus(err) == websocket.StatusNormalClosure {
-			return
-		}
-		if err != nil {
-			fmt.Printf("failed to echo with %v: %v", r.RemoteAddr, err)
-			return
-		}
-	}
-}
+    for {
+        typ, r, err := conn.Reader(ctx)
+        if err != nil {
+            log.Println("Failed to get reader:", err)
+            return
+        }
 
-// echo reads from the WebSocket connection and then writes
-// the received message back to it.
-// The entire function has 10s to complete.
-func echo(c *websocket.Conn, l *rate.Limiter) error {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-	defer cancel()
+        w, err := conn.Writer(ctx, typ)
+        if err != nil {
+            log.Println("Failed to get writer:", err)
+            return
+        }
 
-	err := l.Wait(ctx)
-	if err != nil {
-		return err
-	}
+        buf := new(bytes.Buffer)
+        buf.ReadFrom(r)
+        log.Println(buf.String())
 
-	typ, r, err := c.Reader(ctx)
-	if err != nil {
-		return err
-	}
+        _, err = io.Copy(w, r)
+        if err != nil {
+            log.Println("Failed to io.Copy:", err)
+            return
+        }
 
-	w, err := c.Writer(ctx, typ)
-	if err != nil {
-		return err
-	}
-
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(r)
-	log.Println(buf.String())
-
-	_, err = io.Copy(w, r)
-	if err != nil {
-		return fmt.Errorf("failed to io.Copy: %w", err)
-	}
-
-	err = w.Close()
-	return err
+        err = w.Close()
+        if err != nil {
+            log.Println("Failed to close writer:", err)
+            return
+        }
+    }
 }
