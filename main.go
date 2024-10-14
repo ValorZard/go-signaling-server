7227751436fc69b3029e09b2ae6594e9ab2e695b
Srayan Jana <valorzard.kiko@outlook.com>
2024-10-14 13:25:15 -0700

    remove unnecessary endpoints

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -69,11 +68,6 @@ func main() {
 	mux.Handle("/", http.FileServer(http.Dir("./public")))
 	mux.HandleFunc("/hostLobby", lobbyHost)
 	mux.HandleFunc("/lobby", lobbyHandler)
-	mux.HandleFunc("/offer/get", offerGet)
-	mux.HandleFunc("/offer/post", offerPost)
-	mux.HandleFunc("/answer/get", answerGet)
-	mux.HandleFunc("/answer/post", answerPost)
-	mux.HandleFunc("/ice", ice)
 
 	fmt.Println("Server started on port 3000")
 	// cors.Default() setup the middleware with default options being
@@ -168,82 +162,4 @@ func lobbyHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	lobby.sendTextToHost(r.Context(), body)
-}
-
-func offerGet(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	// get last element of offer slice
-	if len(offers) == 0 {
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte("404 - Offer not found"))
-		return
-	}
-	offer := offers[len(offers)-1]
-
-	jsonValue, _ := json.Marshal(offer)
-
-	io.Writer.Write(w, jsonValue)
-
-	fmt.Println("offerGet")
-	fmt.Println(jsonValue)
-}
-
-func offerPost(w http.ResponseWriter, r *http.Request) {
-	var sdp webrtc.SessionDescription
-
-	// Try to decode the request body into the struct. If there is an error,
-	// respond to the client with the error message and a 400 status code.
-	err := json.NewDecoder(r.Body).Decode(&sdp)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	offers = append(offers, sdp)
-
-	fmt.Println("offerPost")
-	fmt.Println(offers)
-}
-
-func answerGet(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	// get last element of offer slice
-	if len(answers) == 0 {
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte("404 - Answer not found"))
-		return
-	}
-
-	answer := answers[len(answers)-1]
-
-	jsonValue, _ := json.Marshal(answer)
-
-	io.Writer.Write(w, jsonValue)
-
-	fmt.Println("answerGet")
-	fmt.Println(jsonValue)
-}
-
-func answerPost(w http.ResponseWriter, r *http.Request) {
-	var sdp webrtc.SessionDescription
-
-	// Try to decode the request body into the struct. If there is an error,
-	// respond to the client with the error message and a 400 status code.
-	err := json.NewDecoder(r.Body).Decode(&sdp)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	answers = append(answers, sdp)
-
-	fmt.Println("answerPost")
-	fmt.Println(answers)
-}
-
-func ice(w http.ResponseWriter, r *http.Request) {
-	// TODO: Implement the ice handler
-	w.Header().Set("Content-Type", "application/json")
 }
