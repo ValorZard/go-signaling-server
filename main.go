c20102171962159e61ad1f9f9468c1ae93fba16c
Srayan Jana <valorzard.kiko@outlook.com>
2024-10-19 16:51:15 -0700

    client randomly "unappends" from the array. Appears on lobbyJoin then disspears on validatePlayer

--- main.go
+++ main.go
@@ -13,9 +13,6 @@ import (
 	"github.com/rs/cors"
 )
 
-var offers = []webrtc.SessionDescription{}
-var answers = []webrtc.SessionDescription{}
-
 type ClientConnection struct {
 	IsHost bool
 	Offer webrtc.SessionDescription
@@ -23,10 +20,12 @@ type ClientConnection struct {
 }
 
 type Lobby struct {
+	// host is first client in lobby.Clients
 	Clients []ClientConnection
 }
 
 type PlayerData struct {
+	// player id is index in lobby.Clients
 	Id int
 }
 
@@ -92,11 +91,12 @@ func lobbyHost(w http.ResponseWriter, r *http.Request) {
 
 // call "/lobby?id={lobby_id}" to connect to lobby
 func lobbyJoin(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("lobbyJoin")
 	w.Header().Set("Content-Type", "application/json")
 	// https://freshman.tech/snippets/go/extract-url-query-params/
 	// get lobby id from query params
 	lobby_id := r.URL.Query().Get("id")
-	fmt.Printf("lobby_id: %s", lobby_id)
+	fmt.Printf("lobby_id: %s\n", lobby_id)
 
 	// only continue with connection if lobby exists
 	lobby, ok := lobby_list[lobby_id]
@@ -117,37 +117,18 @@ func lobbyJoin(w http.ResponseWriter, r *http.Request) {
 
 	// send player id once generated
 	player_id := len(lobby.Clients)
-	fmt.Printf("player_id: %d", player_id)
+	fmt.Printf("player_id: %d\n", player_id)
 	lobby.Clients = append(lobby.Clients, ClientConnection{IsHost: false})
+	fmt.Println(lobby.Clients)
 	player_data := PlayerData{Id: player_id}
 	jsonValue, _ := json.Marshal(player_data)
 	io.Writer.Write(w, jsonValue)
 }
 
-func offerGet(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	// get last element of offer slice
-	if len(offers) == 0 {
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte("404 - Offer not found"))
-		return
-	}
-	offer := offers[len(offers)-1]
-
-	jsonValue, _ := json.Marshal(offer)
-
-	io.Writer.Write(w, jsonValue)
-
-	/*
-	fmt.Println("offerGet")
-	fmt.Println(jsonValue)
-	*/
-}
-
 func validatePlayer(w http.ResponseWriter, r *http.Request) (string, int, error) {
+	fmt.Println("validatePlayer")
 	lobby_id := r.URL.Query().Get("lobby_id")
-	fmt.Printf("lobby_id: %s\n", lobby_id)
+	//fmt.Printf("lobby_id: %s\n", lobby_id)
 
 	// only continue with connection if lobby exists
 	_, ok := lobby_list[lobby_id]
@@ -161,17 +142,46 @@ func validatePlayer(w http.ResponseWriter, r *http.Request) (string, int, error)
 	player_id_string := r.URL.Query().Get("player_id")
 	player_id, err := strconv.Atoi(player_id_string)
     if err != nil {
-        // ... handle error
-        panic(err)
+        w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("404 - Player not found"))
+		return "", 0, errors.New("Player not found")
     }
-	fmt.Printf("player_id: %d\n", player_id)
+	//fmt.Printf("player_id: %d\n", player_id)
+	//fmt.Printf("length of lobby.Clients: %d\n", len(lobby_list[lobby_id].Clients))
+	fmt.Println(lobby_list[lobby_id].Clients)
+	// check if player actually exists
+	if player_id < 0 || player_id >= len(lobby_list[lobby_id].Clients) {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("404 - Player not found"))
+		return "", 0, errors.New("Player not found")
+	}
 	return lobby_id, player_id, nil
+}
+
+func offerGet(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	lobby_id, player_id, err := validatePlayer(w, r)
+	if err != nil {
+		return
+	}
+
+	offer := lobby_list[lobby_id].Clients[player_id].Offer
+
+	jsonValue, _ := json.Marshal(offer)
+
+	io.Writer.Write(w, jsonValue)
+
+	/*
+	fmt.Println("offerGet")
+	fmt.Println(jsonValue)
+	*/
 }
 
 func offerPost(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("offerPost")
 	
-	_, _, err := validatePlayer(w, r)
+	lobby_id, player_id, err := validatePlayer(w, r)
 	if err != nil {
 		return
 	}
@@ -186,52 +196,45 @@ func offerPost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	offers = append(offers, sdp)
-
-	/*
-	fmt.Println(offers)
-	*/
+	lobby_list[lobby_id].Clients[player_id].Offer = sdp
+	fmt.Println(lobby_list[lobby_id])
 }
 
 func answerGet(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	// get last element of offer slice
-	if len(answers) == 0 {
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte("404 - Answer not found"))
+	lobby_id, player_id, err := validatePlayer(w, r)
+	if err != nil {
 		return
 	}
 
-	answer := answers[len(answers)-1]
+	answer := lobby_list[lobby_id].Clients[player_id].Answer
 
 	jsonValue, _ := json.Marshal(answer)
 
 	io.Writer.Write(w, jsonValue)
-
-	/*
-	fmt.Println("answerGet")
-	fmt.Println(jsonValue)
-	*/
 }
 
 func answerPost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	lobby_id, player_id, err := validatePlayer(w, r)
+	if err != nil {
+		return
+	}
+
 	var sdp webrtc.SessionDescription
 
 	// Try to decode the request body into the struct. If there is an error,
 	// respond to the client with the error message and a 400 status code.
-	err := json.NewDecoder(r.Body).Decode(&sdp)
+	err = json.NewDecoder(r.Body).Decode(&sdp)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	answers = append(answers, sdp)
-
-	/*
-	fmt.Println("answerPost")
-	fmt.Println(answers)
-	*/
+	lobby_list[lobby_id].Clients[player_id].Answer = sdp
+	fmt.Println(lobby_list[lobby_id])
 }
 
 func ice(w http.ResponseWriter, r *http.Request) {
